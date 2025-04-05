0403a609999b41c85c39c788a31895aed5166711
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-05 22:56:46 +0300

    Update defenition.h to v2.1

--- defenition.h
+++ defenition.h
@@ -1,10 +1,15 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <mutex>
 #include <SFML/Graphics.hpp>
+#include "TypeAutomaton.h"
 class Def {
 public:
 	static sf::Vector2f windowsize;
-	static int TPS, FPS, RUN, SIZET;
-	static sf::Color Grey, darkGrey;
+	static int TPS, FPS, RUN, SIZET, running1, running2, CLOSE;
+	static sf::Color Grey, darkGrey, brightRed;
+	static sf::Image buttons, background;
+	static TypeAutomaton mode, liveA, cavesA, mazeA, daynightA, diamobaA, carpetA;
 };
