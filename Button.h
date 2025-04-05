f384be53961a25ccb19bc41ae7898990c3be8d34
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-05 22:50:45 +0300

    Update Button.h to v2.1

--- Button.h
+++ Button.h
@@ -2,14 +2,24 @@
 #include <iostream>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <map>
 #include "functions.h"
+#include "defenition.h"
+
 class Button {
 public: 
     static std::vector <Button*> List;
-    Button(sf::RenderWindow* w, std::string str1, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)());
-    void render(), check();
+    Button(sf::RenderWindow* w, std::string str1, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)(), bool texture_on=true);
+    void render(), check(), draw(int num=-1);
     bool ispressed();
+    virtual void callfunc();
+    std::vector <std::string> active = { "RUN", "STOP", "LIVE", "CARPET", "MAZE", "CAVES", "DAYNIGHT", "DIAMOEBA" };
+    std::map <std::string, TypeAutomaton> mapactive = { {"LIVE", Def::liveA}, {"CARPET", Def::carpetA}, {"DIAMOEBA", Def::diamobaA},\
+        {"DAYNIGHT", Def::daynightA}, { "MAZE", Def::mazeA }, { "CAVES", Def::cavesA } };
+    sf::RenderWindow* w1;
 private:
+    sf::Sprite sprites[4];
+    sf::Texture texture;
     sf::Rect <float> rect;
     sf::RectangleShape rectshape[4];
     sf::Vector2f coord, size;
@@ -17,7 +27,15 @@ private:
     sf::Text text;
     sf::Font font;
     std::string title;
-    sf::RenderWindow* w1;
-    bool pressed;
+    bool pressed, texture_on;
     void (*func)();
+};
+
+class Button1: public Button {
+public:
+    Button1(sf::RenderWindow* w, std::string str1, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)(sf::RenderWindow* w));
+    void callfunc() override;
+
+private:
+    void (*func)(sf::RenderWindow* w1);
 };
