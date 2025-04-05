fe32ff1fac05fe42b88c67a2691206dd3eed4a44
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-05 22:54:26 +0300

    Update Table.h to v2.1

--- Table.h
+++ Table.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <atomic>
 #include "CellAutomaton.h"
 #include "TypeAutomaton.h"
 #include "defenition.h"
@@ -7,7 +9,8 @@
 
 class Table {
 public:
-	float zoom = 1;
+	//std::atomic<int> *zzz;
+	int zoom = 1;
 	bool pressed = false;
 	int brush = 1;
 	TypeAutomaton mode;
@@ -18,7 +21,7 @@ public:
 	Table();
 	Table(sf::RenderWindow* w1, int width, int height);
 	void setmatrix(std::vector<std::vector<int>>* M), render(), nextgeneration(), setrandom(),\
-		clean(), check(), setcustom(), exit();
+		clean(), check(), setcustom();
 	bool ispressed(), myintersects(sf::Rect <float> a, sf::Rect<float> b );
 	std::vector<std::vector<int>> getmatrix();
 private:
