807cd95ce65a0c0e59fa583243539c892b823c4e
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-13 16:46:48 +0300

    Update Button.h to v2.2

--- Button.h
+++ Button.h
@@ -3,19 +3,20 @@
 #include <string>
 #include <SFML/Graphics.hpp>
 #include <map>
+#include "Text.h"
 #include "functions.h"
 #include "defenition.h"
 
 class Button {
 public: 
     static std::vector <Button*> List;
-    Button(sf::RenderWindow* w, std::string str1, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)(), bool texture_on=true);
-    void render(), check(), draw(int num=-1);
-    bool ispressed();
+    virtual void init(sf::RenderWindow* w, std::string str1, sf::Font* font, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)(), bool texture_on=true) final;
+    void render(), draw(int num=-1);
+    virtual void check();
+    int ispressed(); // 0 - мышь не на кнопке, 1 - мышь на кнопке и отжата, 2 - мышь на кнопке и нажата
     virtual void callfunc();
-    std::vector <std::string> active = { "RUN", "STOP", "LIVE", "CARPET", "MAZE", "CAVES", "DAYNIGHT", "DIAMOEBA" };
-    std::map <std::string, TypeAutomaton> mapactive = { {"LIVE", Def::liveA}, {"CARPET", Def::carpetA}, {"DIAMOEBA", Def::diamobaA},\
-        {"DAYNIGHT", Def::daynightA}, { "MAZE", Def::mazeA }, { "CAVES", Def::cavesA } };
+    static std::vector <std::string> active;
+    static std::map <std::string, TypeAutomaton> mapactive;
     sf::RenderWindow* w1;
 private:
     sf::Sprite sprites[4];
@@ -24,18 +25,12 @@ private:
     sf::RectangleShape rectshape[4];
     sf::Vector2f coord, size;
     sf::Color colbut, coltext;
-    sf::Text text;
-    sf::Font font;
+    Text text;
+    sf::Font* font;
+    int sizetext;
     std::string title;
-    bool pressed, texture_on;
+    bool texture_on;
+    int pressed = 0;
     void (*func)();
 };
-
-class Button1: public Button {
-public:
-    Button1(sf::RenderWindow* w, std::string str1, sf::Vector2f coord1, sf::Vector2f size1, int sizetext, sf::Color col1, sf::Color col_text1, void (*func)(sf::RenderWindow* w));
-    void callfunc() override;
-
-private:
-    void (*func)(sf::RenderWindow* w1);
-};
+;
