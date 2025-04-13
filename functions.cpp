cebed27c0514522e587608df14801d26ad7ab26f
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-13 16:52:49 +0300

    Update functions.cpp to v2.2

--- functions.cpp
+++ functions.cpp
@@ -3,3 +3,6 @@
 sf::Vector2f get_mouse_pos(sf::RenderWindow* w1) {
 	return { (float)sf::Mouse::getPosition(*w1).x, (float)sf::Mouse::getPosition(*w1).y };
 }
+
+
+int getrandom(int a, int b) { return rand() % (b - a + 1) + a; }
