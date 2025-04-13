0db11449d1ea94b2b655aa39acfb18f69cc077eb
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-13 16:48:14 +0300

    Update CellAutomaton.h to v2.2

--- CellAutomaton.h
+++ CellAutomaton.h
@@ -9,8 +9,9 @@ class CellAutomaton {
 public:
 	static int SIZET;
 	std::vector <std::vector<int>> M;
+	std::vector<std::pair<sf::Vector2f, int>> update;
 	CellAutomaton();
 	static int getsizet();
 	int getrandom(int a, int b), getcntnbrs(int i, int j);
-	void nextgeneration(TypeAutomaton mode), printmatrix(), clean(), setrandom();
+	void nextgeneration(TypeAutomaton mode), printmatrix(), clean(), setrandom(), setcustom();
 };
