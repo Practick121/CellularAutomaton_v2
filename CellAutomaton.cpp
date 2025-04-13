26a1213d8f715c528f2e1252cd1e4c972cdc94c3
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-13 16:47:35 +0300

    Update CellAutomaton.cpp to v2.2

--- CellAutomaton.cpp
+++ CellAutomaton.cpp
@@ -2,36 +2,54 @@
 int CellAutomaton::SIZET = Def::SIZET;
 int CellAutomaton::getrandom(int a, int b) { return rand() % (b - a + 1) + a; }
 int CellAutomaton::getsizet() { return SIZET; }
-CellAutomaton::CellAutomaton() { M.assign(SIZET + 2, std::vector<int>(SIZET + 2, 0)); }
+CellAutomaton::CellAutomaton() { M.assign(SIZET, std::vector<int>(SIZET, 0)); }
 void CellAutomaton::setrandom() {
-    for (int i = 1; i <= SIZET; i++)
-        for (int j = 1; j <= SIZET; j++)
-            M[i][j] = getrandom(0, 1);
+    for (float i = 0; i < SIZET; i++)
+        for (float j = 0; j < SIZET; j++)
+			update.push_back({ { i, j }, (int)(getrandom(1, 100) <= Def::chance)});
 }
 int CellAutomaton::getcntnbrs(int i, int j) {
-    return M[i - 1][j - 1] + M[i - 1][j] + M[i - 1][j + 1] + M[i][j - 1] + M[i][j + 1]\
-        + M[i + 1][j - 1] + M[i + 1][j] + M[i + 1][j + 1];
+
+    int ans = 0;
+	int ipast = (i - 1 + SIZET) % SIZET, jpast = (j - 1 + SIZET) % SIZET;
+	int inext = (i + 1 + SIZET) % SIZET, jnext = (j + 1 + SIZET) % SIZET;
+
+    if (Def::typecheckingneighbours == 4)
+        return M[ipast][j] + M[i][jpast] + M[i][jnext] + M[inext][j];
+    else
+	    return M[ipast][jpast] + M[ipast][j] + M[ipast][jnext] + M[i][jpast] + M[i][jnext] + M[inext][jpast] + M[inext][j] + M[inext][jnext];
 }
 void CellAutomaton::nextgeneration(TypeAutomaton mode) {
-    std::vector<std::vector<int>> M2(SIZET + 2, std::vector<int>(SIZET + 2, 0));
     std::unordered_set<int> surv = mode.getsurvive(), birth = mode.getbirth();
-    for (int i = 1; i <= SIZET; i++) {
-        for (int j = 1; j <= SIZET; j++) {
-            //int cnt = getRandom_num(1, 8);
+    for (float i = 0; i < SIZET; i+=1) {
+        for (float j = 0; j < SIZET; j+=1) {
             int cnt = getcntnbrs(i, j);
             if (M[i][j] == 0 && birth.find(cnt) != birth.end())
-                M2[i][j] = 1;
-            else if (M[i][j] == 1 && surv.find(cnt) != surv.end())
-                M2[i][j] = 1;
+                update.push_back({ { i, j }, 1 });
+            else if (M[i][j] == 1 && surv.find(cnt) == surv.end())
+                update.push_back({ { i, j }, 0 });
         }
     }
-    for (int i = 1; i <= SIZET; i++)
-        for (int j = 1; j <= SIZET; j++)
-            M[i][j] = M2[i][j];
+}
+void CellAutomaton::setcustom() {
+    for (float i = 0; i < SIZET; i++) {
+        for (float j = 0; j < SIZET; j++) {
+            /*if (j <= my.getSizeT() / 2)
+                my.M[i][j] = 0;
+            else
+                my.M[i][j] = 1;*/
+            int dl = 10;
+            if (i <= SIZET / 2 + dl && i >= SIZET / 2 - dl\
+                && j <= SIZET / 2 + dl && j >= SIZET / 2 - dl)
+                update.push_back({ { i, j }, (int)(Def::chance >= getrandom(1, 100)) });
+            else
+				update.push_back({ { i, j }, 0 });
+        }
+    }
 }
 void CellAutomaton::printmatrix() {
-    for (int i = 1; i <= SIZET; i++) {
-        for (int j = 1; j <= SIZET; j++) {
+    for (int i = 0; i < SIZET; i++) {
+        for (int j = 0; j < SIZET; j++) {
             std::cout << M[i][j];
         }
         std::cout << std::endl;
@@ -39,7 +57,10 @@ void CellAutomaton::printmatrix() {
     std::cout << std::endl;
 }
 void CellAutomaton::clean() {
-    for (int i = 1; i <= SIZET; i++)
-        for (int j = 1; j <= SIZET; j++)
+    for (float i = 0; i < SIZET; i++) {
+        for (float j = 0; j < SIZET; j++) {
             M[i][j] = 0;
+			update.push_back({ { i, j }, 0 });
+        }
+    }
 }
