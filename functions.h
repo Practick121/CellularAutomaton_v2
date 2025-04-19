023f1854818e92f8666769607927110cbb005521
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-20 00:55:43 +0300

    Update functions.h to v2.3

--- functions.h
+++ functions.h
@@ -1,8 +1,17 @@
 #pragma once
-
-#include <SFML/Graphics.hpp>
 #include "TypeAutomaton.h"
-#include "defenition.h"
+#include "Global.h"
+#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
 
 sf::Vector2f get_mouse_pos(sf::RenderWindow* w);
+sf::Vector2f get_mouse_pos_desktop();
+
 int getrandom(int a, int b);
+
+void generate_automaton();
+
+void Close(sf::RenderWindow* iw);
+
+std::vector<int> string_to_mas(std::string s, char sep=' ');
