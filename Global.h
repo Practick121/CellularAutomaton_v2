a14d1a32b69ebd4b4195893d6cc50610ba671ecb
DanBeresnev <122594142+Practick121@users.noreply.github.com>
2025-04-20 01:03:54 +0300

    Create Global.h (v2.3)

---
+++ Global.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "TypeAutomaton.h"
+#include "Config.h"
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <SFML/Graphics.hpp>
+
+class Gl {
+public:
+	static int RUN, running, CLOSE, CNT_GENER;
+	static sf::Color Grey, darkGrey, brightRed;
+	static sf::Image buttons, background;
+	static bool focused;
+	static TypeAutomaton mode, tempA;
+	static std::string input;
+	static std::thread rendering;
+};
